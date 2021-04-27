125026d4188ea04b2f55ba1b65839151d3a8726d
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-27 09:51:20 -0600

    for active nav items

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -2,7 +2,7 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-import {Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route, Link, useMatch} from 'react-router-dom'
 import {Button} from './components/lib'
 import * as mq from './styles/media-queries'
 import * as colors from './styles/colors'
@@ -55,9 +55,10 @@ function AuthenticatedApp({user, logout}) {
 }
 
 function NavLink(props) {
+  const match = useMatch(props.to)
   return (
     <Link
-      css={{
+      css={[{
         display: 'block',
         padding: '8px 15px 8px 10px',
         margin: '5px 0',
@@ -71,7 +72,12 @@ function NavLink(props) {
           textDecoration: 'none',
           background: colors.gray10,
         },
-      }}
+      }, match ? 
+          {borderLeft: `5px solid ${colors.indigo}`,
+  background: colors.gray10,
+  ':hover': {
+    background: colors.gray20,
+  },} : null]}
       {...props}
     />
   )
